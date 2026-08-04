bc10be30776430c3a231e553469cc8a53d6726fb
ARH Prints <arhorderhub@gmail.com>
2026-08-04 17:19:37 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,1 @@
-export const EMBEDDED_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzO5yEvRtj7mCJtbSsHvacfsndSuk5oXX_xl75Hr5r-Dm1t35JAPR9RckCGNyXG7VE_hQ/exec';
+export const EMBEDDED_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbx76935uUHFRlQnSYsvAIPLZ57cSHLp3z7GjHl377j_zs5wdpzTR2wHAFbY9T4wTnOE/exec';
